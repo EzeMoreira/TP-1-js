3aab6f7fb067317a4ca374864e7e98e05941b3bf
eze moreira <eze@example.com>
2023-03-09 21:08:14 -0300

    first commit

--- TP-1-js.js
+++ TP-1-js.js
@@ -1,3 +1,77 @@
+        //Ejercicio 1
+
 //alert("un mensaje");
 
-document.write("Hello World");
+        //Ejercicio 2
+//document.write("Hello World");
+
+        //Ejercicio 3
+//document.write("", ( 3 + 5 ));
+
+        //Ejercicio 4
+//let NombreDeUsuario = prompt("Ingrese su nombre de usuario")
+//document.write("Hola" + NombreDeUsuario)
+
+        //Ejercicio 5
+// let numberone = prompt("Escribe numberone") 
+// let numbertwo = prompt("Escribe numbertwo")
+// document.write (parseInt(numberone) + parseInt(numbertwo))
+
+// parseint funciona para convertir el valor ingresado de cadena de texto (string) en un num entero
+
+        //Ejercicio 6
+// let num1 = parseInt(prompt("Escríbe un numero"))
+// let num2 = parseInt(prompt("Escríbe un numero"))
+
+// if (num1 > num2) {
+//     document.write (num1)
+// } else  { document.write (num2) };
+
+        //Ejercicio 7
+// let nu1 = prompt("Ingresa un numero");
+// let nu2 = prompt("Ingresa un numero");
+// let nu3 = prompt("Ingresa un numero");
+
+// if (nu1 > nu2 && nu1 > nu3) {
+//     document.write(nu1)
+// } else { (nu2 > nu3)
+//     document.write(nu2) }
+
+        //Ejercicio 8
+// let numero = prompt("Ingrese un numero");
+
+// if (numero % 2 == 0) {
+//     document.write("Es divisible en 2")
+// } else { 
+//     document.write("No es divisible en 2")
+// }
+
+        //Ejercicio 9
+// let frase = prompt("escribe una frase");
+
+// for (let index = 0; index < frase.length; index++) {
+//     const letra = frase.charAt(index)
+
+//     if (
+//         letra === "a" ||    letra === "e" ||    letra === "i" ||    letra === "o" ||    letra === "u"
+//     ) {
+//         document.write(letra);     
+//     }
+// };
+
+        //Ejercicio 10-11
+// let numero = prompt("cansado de los numeros? yo igual, pone tu numero")
+
+// if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0) {
+//     if (numero % 2 == 0) {
+//         document.write("Si es divisible capo, segui...")
+//     } if (numero % 3 == 0) {
+//         document.write("Si es divisible capo, segui...")
+//     } if (numero % 5 == 0) {
+//         document.write("Si es divisible capo, segui...")
+//     } if (numero % 7 == 0) {
+//         document.write("Si es divisible capo, segui...")
+//     } else {
+//         document.write("Para capo, aqui ya no")
+//     }
+// }
